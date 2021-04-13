28e7b76e1dbdf333c7ecf3448c3b09accca33a41
Ablan Abkenov <abkenov.ablan@gmail.com>
2021-04-13 21:10:04 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [from, setFrom] = useState();
+  const [fromCurrency, setFromCurrency] = useState('USD');
+  const [toCurrency, setToCurrency] = useState('KZT');
+  const [result, setResult] = useState(0);
+  const [rates, setRates] = useState({});
+
+  const currencies = ['USD', 'KZT', 'GBP', 'RUB', 'EUR']
+
+  const baseURL =
+    'http://api.exchangeratesapi.io/v1/latest?access_key=02fc92d50ffe2b124d04cfe86c2f6a4b&symbols=EUR,USD,KZT,GBP,RUB';
+
+  async function updateCurrency(url) {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const json = await response.json();
+      const data = JSON.parse(JSON.stringify(json.rates));
+
+      setRates({...rates, data})
+    }
+  }
+
+  useEffect(() => {
+    updateCurrency(baseURL);
+  }, []);
+
+  function handleConvert() {
+    const temp = rates.data[toCurrency] / rates.data[fromCurrency] * from
+    setResult(temp)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>My converter</h1>
+      <div className='converter-window'>
+          <input type='number' value={from} onChange={e => setFrom(e.target.value)} align='middle'></input>
+          <select value={fromCurrency} onChange={e => setFromCurrency(e.target.value)}>
+            {currencies.map(item => <option key={item}>{item}</option>)}
+          </select>
+          <select value={toCurrency} onChange={e => setToCurrency(e.target.value)}>
+            {currencies.map(item => <option key={item}>{item}</option>)}
+          </select>
+      </div>
+      <h3 className='result'>{result}</h3>
+      <button className='convert-button' onClick={() => handleConvert()}>Convert</button>
     </div>
   );
 }
